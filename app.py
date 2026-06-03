9fa48c3c99c578edca9bf483cd664157a859a15c
jabjeestudio-ctrl <jabjeestudio@gmail.com>
2026-06-03 15:06:12 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import cv2
+import numpy as np
+import faiss
+import pickle
+from insightface.app import FaceAnalysis
+import os
+
+@st.cache_resource
+def load_resources():
+    index = faiss.read_index("event.index")
+    with open("image_paths.pkl", "rb") as f:
+        image_paths = pickle.load(f)
+    app = FaceAnalysis(providers=['CPUExecutionProvider'])
+    app.prepare(ctx_id=0, det_size=(640, 640))
+    return index, image_paths, app
+
+index, image_paths, app = load_resources()
+
+st.title("ระบบสแกนใบหน้าค้นหารูป")
+uploaded_file = st.file_uploader("อัปโหลดรูปของคุณ", type=['jpg', 'jpeg', 'png'])
+
+if uploaded_file is not None:
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    img_guest = cv2.imdecode(file_bytes, 1)
+    
+    guest_faces = app.get(img_guest)
+    if guest_faces:
+        guest_emb = guest_faces[0].normed_embedding.reshape(1, -1)
+        distances, indices = index.search(guest_emb.astype('float32'), k=5)
+        
+        st.success("พบรูปที่ใกล้เคียงกับคุณ:")
+        for idx in indices[0]:
+            if idx != -1:
+                st.image(image_paths[idx])
+    else:
+        st.warning("ไม่พบใบหน้าในรูป")
